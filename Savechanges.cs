0adfa20daf0fa5511515b0f01156a369c32f4bd5
Artis <Artis@DESKTOP-03P96RB>
2025-05-18 14:08:53 +0300

    Additions

---
+++ Savechanges.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursadarbs
+{
+    /// <summary>
+    /// TODO: Make a save class that works across forms. Doing it in each form will be ass.
+    /// </summary>
+    class Savechanges
+    {
+    }
+}
